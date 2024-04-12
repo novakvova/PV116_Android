cc1e8aff6b465f2e38c51f082c80dff1d3decdde
novakvova <novakvova@gmail.com>
2024-04-12 19:15:44 +0300

    Add-Migration

---
+++ WebKrot/Infrastructure/Data/DataBaseContext.cs
@@ -0,0 +1,22 @@
+﻿using Core.Entities;
+using Infrastructure.EntitiesConfiguration;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data
+{
+    public class DataBaseContext : DbContext
+    {
+        public DataBaseContext(DbContextOptions<DataBaseContext> options)
+            : base(options)
+        {
+
+        }
+        public DbSet<CategoryEntity> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.ApplyConfiguration(new CategoryConfiguration());
+        }
+    }
+}
--- WebKrot/WebKrot/Program.cs
+++ WebKrot/WebKrot/Program.cs
@@ -1,13 +1,26 @@
 using Microsoft.Extensions.FileProviders;
+using Infrastructure;
+using Core;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+string connection = builder.Configuration.GetConnectionString("ConnectionSqlite");
+
+builder.Services.AddDBContext(connection);
 
 builder.Services.AddControllers();
+
+builder.Services.AddRepository();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddValidator();
+
+builder.Services.AddAutoMapper();
+
+builder.Services.AddCustomService();
 
 var app = builder.Build();
 
