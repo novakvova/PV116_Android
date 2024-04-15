4a9595a85fa4b01623fcb7433f91b05a05ffafc9
novakvova <novakvova@gmail.com>
2024-04-15 20:26:56 +0300

    Begin working view categories

--- AndroidApp/app/src/main/java/com/example/myapp/MainActivity.java
+++ AndroidApp/app/src/main/java/com/example/myapp/MainActivity.java
@@ -1,20 +1,33 @@
 package com.example.myapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.RequestOptions;
+import com.example.myapp.category.CategoriesAdapter;
+import com.example.myapp.dto.category.CategoryItemDTO;
+import com.example.myapp.services.ApplicationNetwork;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
+    RecyclerView rcCategories;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        /*
         ImageView ivAvatar = findViewById(R.id.imageView);
 //        String url = "https://i.pinimg.com/564x/a8/4b/94/a84b94d816597cea9c83cb64d9d7e0e7.jpg";
 //        String url = "http://10.0.2.2:5094/images/1.jpg";
@@ -23,5 +36,28 @@ public class MainActivity extends AppCompatActivity {
                 .load(url)
                 .apply(new RequestOptions().override(400))
                 .into(ivAvatar);
+*/
+
+        rcCategories = findViewById(R.id.rcCategories);
+        rcCategories.setHasFixedSize(true);
+        rcCategories.setLayoutManager(new GridLayoutManager(this, 1, RecyclerView.VERTICAL, false));
+
+        ApplicationNetwork
+                .getInstance()
+                .getCategoriesApi()
+                .list()
+                .enqueue(new Callback<List<CategoryItemDTO>>() {
+                    @Override
+                    public void onResponse(Call<List<CategoryItemDTO>> call, Response<List<CategoryItemDTO>> response) {
+                        List<CategoryItemDTO> items = response.body();
+                        //Log.d("--List categories--", String.valueOf(items.size()));
+                        CategoriesAdapter ca = new CategoriesAdapter(items);
+                        rcCategories.setAdapter(ca);
+                    }
+                    @Override
+                    public void onFailure(Call<List<CategoryItemDTO>> call, Throwable throwable) {
+
+                    }
+                });
     }
 }
---
+++ AndroidApp/app/src/main/java/com/example/myapp/network/CategoriesApi.java
@@ -0,0 +1,13 @@
+package com.example.myapp.network;
+
+import com.example.myapp.dto.category.CategoryItemDTO;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface CategoriesApi {
+    @GET("/api/CategoryControllers/CategoryGetAsync")
+    public Call<List<CategoryItemDTO>> list();
+}
