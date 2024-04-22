99c45cbb36a431de003bb0ddb357cc0ed00479e3
novakvova <novakvova@gmail.com>
2024-04-22 18:47:43 +0300

    Begin working app

---
+++ AndroidApp/app/src/main/java/com/example/myapp/category/CategoryCreateActivity.java
@@ -0,0 +1,16 @@
+package com.example.myapp.category;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.myapp.R;
+
+public class CategoryCreateActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_category_create);
+    }
+}
--- AndroidApp/app/src/main/java/com/example/myapp/network/CategoriesApi.java
+++ AndroidApp/app/src/main/java/com/example/myapp/network/CategoriesApi.java
@@ -3,11 +3,23 @@ package com.example.myapp.network;
 import com.example.myapp.dto.category.CategoryItemDTO;
 
 import java.util.List;
+import java.util.Map;
 
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Multipart;
+import retrofit2.http.POST;
+import retrofit2.http.Part;
+import retrofit2.http.PartMap;
 
 public interface CategoriesApi {
-    @GET("/api/CategoryControllers/CategoryGetAsync")
+    @GET("/api/Category/CategoryGetAsync")
     public Call<List<CategoryItemDTO>> list();
+
+    @Multipart
+    @POST("/api/category/createcategory")
+    public Call<CategoryItemDTO> create(@PartMap Map<String, RequestBody> params,
+                                        @Part MultipartBody.Part image);
 }
